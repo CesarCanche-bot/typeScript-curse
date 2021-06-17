d8bf4ea00aaf3fde21d97d8a05355479a52378e6
canche <canche285@gmail.com>
2021-06-16 23:54:45 -0700

    END video 7 Setting up a simple typescript project

---
+++ section2/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiply(a, b) {
+    return a * b;
+}
+var a = 10;
+var b = 2;
+console.log('resut', a + " * " + b + " = " + multiply(a, b));
---
+++ section2/src/index.ts
@@ -0,0 +1,8 @@
+function multiply (a:number, b: number): number {
+    return a*b;
+}
+
+const a = 10;
+const b = 2;
+
+console.log('resut', `${a} * ${b} = ${multiply(a,b)}`);
