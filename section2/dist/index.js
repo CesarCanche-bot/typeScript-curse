ddfc1211b4b5d116c1aa12192fed5d41d420681b
Julio Cesar Canche*2 <julio.canche@ensenada.gob.mx>
2021-06-22 09:12:45 -0700

    ADD some examples

--- section2/dist/index.js
+++ section2/dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
-function multiply(a, b) {
-    return a * b;
-}
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var multiply_1 = __importDefault(require("./multiply"));
 var a = 10;
 var b = 2;
-console.log('resut', a + " * " + b + " = " + multiply(a, b));
+console.log('resut', a + " * " + b + " = " + multiply_1.default(a, b));
